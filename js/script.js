c3859b4620a2665fb0098faf09a36155737543c8
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-01 15:41:50 -0700

    Refactored JS

--- js/script.js
+++ js/script.js
@@ -11,28 +11,52 @@ $(document).ready(function(){
 
   };
 
+  var is_game_over = function() {
+    return ($('.tile.matched').size() == tiles.length);
+  };
+
+  var handle_win = function() {
+    alert('You Win! You found all the matches in ' + num_clicks + ' clicks!');
+    window.location.href = window.location.href;
+  };
+
+  var activate_tile = function(tile) {
+    tile.addClass('active');
+    selected.push(tile);
+  };
+
+  var deactivate_tiles = function(){
+    $('.tile.active').removeClass('active');
+    selected = [];
+  };
+
+  var is_two_selected = function() {
+    return (selected.length == 2);
+  };
+
+  var increment_click_counter = function() {
+    num_clicks++;
+  };
+
   var handle_click = function() {
     var tile = $(this);
     if (tile.hasClass('active') || tile.hasClass('matched')){
       return false;
     }
-    if (selected.length == 2){
-      $('.tile.active').removeClass('active');
-      selected = [];
+    if (is_two_selected()){
+      deactivate_tiles();
     }
-    num_clicks++;
-    tile.addClass('active');
-    selected.push(tile);
+    increment_click_counter();
+    activate_tile(tile);
 
-    if (selected.length == 2 && is_current_selection_a_match()) {
+    if (is_two_selected && is_current_selection_a_match()) {
 
       $.each(selected, function(index, matched_tile) {
         matched_tile.addClass('matched');
       });
 
-      if ($('.tile.matched').size() == tiles.length){
-        alert('You Win! You found all the matches in ' + num_clicks + ' clicks!');
-        window.location.href = window.location.href;
+      if (is_game_over()) {
+        handle_win();
       }
     }
   };

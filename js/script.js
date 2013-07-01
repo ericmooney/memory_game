de70dccc39f52fe94d2c8ae915c5a1e08f695201
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-01 14:21:08 -0700

    Add functionality for 2 clicks per view

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@ $(document).ready(function(){
 
   var tiles = $('.tile');
   var num_clicks = 0;
+  var selected = [];
 
   var handle_click = function() {
     var tile = $(this);
+    if (tile.hasClass('active')){
+      return false;
+    }
+    if (selected.length == 2){
+      $('.tile.active').removeClass('active');
+      selected = [];
+    }
+    num_clicks++;
     tile.addClass('active');
+    selected.push(tile);
   };
 
   $.each(tiles, function(index, tile) {

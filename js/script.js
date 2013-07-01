cc201af2f85a4be5a4baf5af3a83fdc996adc03a
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-01 14:01:08 -0700

    Added to JS so that images will show upon click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
-  console.log(':)')
-})
+
+  var tiles = $('.tile');
+  var num_clicks = 0;
+
+  var handle_click = function() {
+    var tile = $(this);
+    tile.addClass('active');
+  };
+
+  $.each(tiles, function(index, tile) {
+    var tile = $(tile);
+    tile.on('click', handle_click);
+  });
+
+});
